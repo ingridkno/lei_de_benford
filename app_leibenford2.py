4267a5f28188e1c3f8112fb9de7cdf154a5c4df2
Ingrid Knochenhauer de Souza <50635626+ingridkno@users.noreply.github.com>
2022-05-24 20:21:41 -0300

    Update app_leibenford2.py

--- app_leibenford2.py
+++ app_leibenford2.py
@@ -59,8 +59,7 @@ if uploaded_file is not None:
          #     size=0.5
          # ),
          marker_color='#EB89B5',
-         opacity=0.75,
-         texttemplate="%{x:2f}"
+         opacity=0.75
      ))
 
 
